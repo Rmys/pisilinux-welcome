b9d1d604c00c99a3bb83b01f400fe744d6f45d5c
Rmys <muscnsl@gmail.com>
2019-01-18 22:41:03 +0300

    pisilinux-welcome.svg

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ system('pyrcc5 welcome.qrc -o welcome/resource.py')
 
 datas = [('/usr/share/applications', ['data/pisilinux-welcome.desktop']),
          ('/etc/skel/.config/autostart', ['data/pisilinux-welcome.desktop']),
+         ('/usr/share/icons/hicolor/scalable/apps', ['images/pisilinux-welcome.svg']),
          ('/usr/share/welcome/languages', langs)]
 
 setup(
